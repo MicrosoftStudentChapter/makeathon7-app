8d18255e1b78a9f84f505a39ddee03eba7839552
Vatsal Gupta <tsrvatsal17@gmail.com>
2025-01-28 22:40:15 +0530

    initial commit

---
+++ lib/tracks_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:makeathon7/theme/app_pallete.dart';
+
+class TracksPage extends StatelessWidget {
+  const TracksPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppPallete.backgroundColor,
+      
+    );
+  }
+}
