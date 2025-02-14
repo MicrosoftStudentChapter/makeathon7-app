8a8f474910e2acf0594f299fffbb9387958a5265
Khushi Dangi <dangikhushi2006@gmail.com>
2025-02-14 21:05:56 +0530

    login page

--- lib/core/pages/login_page.dart
+++ lib/core/pages/login_page.dart
@@ -10,38 +10,157 @@ class LoginPage extends StatelessWidget {
     return SafeArea(
       child: Container(
         decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('lib/assets/images/background.png'),
-              fit: BoxFit.cover,
-            ),
+          image: DecorationImage(
+            image: AssetImage('lib/assets/images/background.png'),
+            fit: BoxFit.cover,
           ),
+        ),
         child: Scaffold(
           backgroundColor: Colors.transparent,
           body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                'Login Page',
-                style: TextStyle(
-                    color: Colors.white, fontSize: 56, fontFamily: 'IntroRust'),
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                 padding: const EdgeInsets.only(
+                      left: 20.0, top: 0.0),
+                  child: Image.asset(
+                    'lib/assets/images/logo.png', // Path to your logo image
+                    height: 1000, // Adjust the height as needed
+                  ),
+                ),
               ),
-              ElevatedButton(onPressed: () {
-                Navigator.push(context, PageRouteBuilder(
-                            pageBuilder: (context, animation1, animation2) => HomePageNavigator(),
-                            transitionsBuilder: (context, animation1, animation2, child) {
-                              return FadeTransition(
-                                opacity: animation1,
-                                child: child,
-                              );
-                            },
-                            transitionDuration: duration,
-                          ));
-              }, style: ButtonStyle(
-                backgroundColor: WidgetStatePropertyAll(Colors.red),
-              ), child: Text('Login'),),
-              ],
-            ),
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: MediaQuery.of(context).size.width * 0.1),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 10),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Email/Username',
+                            hintStyle: TextStyle(
+                              color: Colors.white.withAlpha(158),
+                              fontFamily: 'Font',
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                              borderRadius: BorderRadius.circular(22.0),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                              borderRadius: BorderRadius.circular(22.0),
+                            ),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                              borderRadius: BorderRadius.circular(22.0),
+                            ),
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 10.0,
+                                horizontal: 20.0), // Decrease height
+                          ),
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 12),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Password',
+                            hintStyle: TextStyle(
+                              color: Colors.white.withAlpha(158),
+                              fontFamily: 'Font',
+                             
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                              borderRadius: BorderRadius.circular(22.0),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                              borderRadius: BorderRadius.circular(22.0),
+                            ),
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 10.0,
+                                horizontal: 20.0), // Decrease height
+                          ),
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                          ),
+                          obscureText: true,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width *
+                            0.5, // Adjust the width of the login button
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation1, animation2) =>
+                            HomePageNavigator(),
+                        transitionsBuilder:
+                            (context, animation1, animation2, child) {
+                          return FadeTransition(
+                            opacity: animation1,
+                            child: child,
+                          );
+                        },
+                        transitionDuration: duration,
+                      ));
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color.fromARGB(255, 154, 7,
+                                0), // Set the background color to red
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                  24.0), // Set the border radius
+                            ),
+                            padding: EdgeInsets.symmetric(
+                                vertical: 10.0), // Adjust the padding
+                          ),
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Font',
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      TextButton(
+                        onPressed: () {
+                          // Add your navigation logic here
+                        },
+                        child: Text(
+                          "don't have an account?",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Font',
+                            fontSize: 17.0,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
+        ),
       ),
     );
   }
