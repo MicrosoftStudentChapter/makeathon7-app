aea31d0b4a905b379bcda20001b73231b7030d69
Vatsal Gupta <tsrvatsal17@gmail.com>
2025-03-16 14:54:09 +0530

    heading alignment

--- lib/core/pages/announcements/announcements.dart
+++ lib/core/pages/announcements/announcements.dart
@@ -48,92 +48,83 @@ class Announcements extends StatelessWidget {
             backgroundColor: Colors.transparent,
             automaticallyImplyLeading: false,
           ),
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.122,
-                  ),
-                  Text(
-                    'Announcements',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 32,
-                        fontFamily: 'IntroRust'),
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.122,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.1,
-              ),
-              Expanded(
-                child: SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.48,
-                  child: ListView.builder(
-                    itemCount: 6,
-                    itemBuilder: (context, index) {
-                      return SingleChildScrollView(
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                SizedBox(
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.1,
-                                ),
-                                Text(
-                                  'Announcement ${index + 1}',
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 23,
-                                      fontWeight: FontWeight.w600,
-                                      fontFamily: 'LeagueSpartan'),
-                                ),
-                                SizedBox(
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.25,
-                                ),
-                                Text(
-                                  '20:48',
-                                  style: TextStyle(
-                                      color: AppPallete.redColor,
-                                      fontSize: 13,
-                                      fontWeight: FontWeight.w600,
-                                      fontFamily: 'LeagueSpartan'),
-                                ),
-                              ],
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 37),
-                              child: Text(
-                                'Checkpoint will begin at 21:00. Please be seated at your seats till then. Meow meow meow Meow meow meow Meow meow meow',
-                                style: TextStyle(
-                                    color: AppPallete.whiteOpac,
-                                    fontSize: 17,
-                                    fontWeight: FontWeight.w600,
-                                    fontFamily: 'LeagueSpartan'),
+          body: Center(
+            child: Column(
+              children: [
+                Text(
+                  'Announcements',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontFamily: 'IntroRust'),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.1,
+                ),
+                Expanded(
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.48,
+                    child: ListView.builder(
+                      itemCount: 6,
+                      itemBuilder: (context, index) {
+                        return SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  SizedBox(
+                                    width:
+                                        MediaQuery.of(context).size.width * 0.1,
+                                  ),
+                                  Text(
+                                    'Announcement ${index + 1}',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 23,
+                                        fontWeight: FontWeight.w600,
+                                        fontFamily: 'LeagueSpartan'),
+                                  ),
+                                  SizedBox(
+                                    width:
+                                        MediaQuery.of(context).size.width * 0.25,
+                                  ),
+                                  Text(
+                                    '20:48',
+                                    style: TextStyle(
+                                        color: AppPallete.redColor,
+                                        fontSize: 13,
+                                        fontWeight: FontWeight.w600,
+                                        fontFamily: 'LeagueSpartan'),
+                                  ),
+                                ],
                               ),
-                            ),
-                            SizedBox(
-                              height: MediaQuery.of(context).size.height * 0.03,
-                            ),
-                          ],
-                        ),
-                      );
-                    },
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 37),
+                                child: Text(
+                                  'Checkpoint will begin at 21:00. Please be seated at your seats till then. Meow meow meow Meow meow meow Meow meow meow',
+                                  style: TextStyle(
+                                      color: AppPallete.whiteOpac,
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.w600,
+                                      fontFamily: 'LeagueSpartan'),
+                                ),
+                              ),
+                              SizedBox(
+                                height: MediaQuery.of(context).size.height * 0.03,
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.22,
-              ),
-            ],
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.22,
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/core/pages/timeline/timeline.dart
+++ lib/core/pages/timeline/timeline.dart
@@ -50,104 +50,96 @@ class TimelinePage extends StatelessWidget {
               ),
             ],
           ),
-          body: Column(
-            children: [
-              Row(
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.15,
-                  ),
-                  Text(
-                    'Timeline',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 56,
-                        fontFamily: 'IntroRust'),
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.15,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.05,
-              ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: AppPallete.timelineBg,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  width: 295,
-                  height: 370,
-                  child: ListView.builder(
-                    itemCount: 6,
-                    itemBuilder: (context, index) {
-                      return SingleChildScrollView(
-                        child: Padding(
-                          padding: const EdgeInsets.all(15.0),
-                          child: Column(
-                            children: [
-                              Row(
-                                children: [
-                                  SizedBox(
-                                    width: MediaQuery.of(context).size.width *
-                                        0.17,
-                                  ),
-                                  Text(
-                                    'Event ${index + 1}',
-                                    style: TextStyle(
-                                        color: AppPallete.timelineGold,
-                                        fontSize: 30.33,
-                                        fontWeight: FontWeight.w600,
-                                        fontFamily: 'LemonMilkMedium'),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  SizedBox(
-                                    width: MediaQuery.of(context).size.width *
-                                        0.195,
-                                  ),
-                                  Text(
-                                    '22/2/2025',
-                                    style: TextStyle(
-                                        color: AppPallete.redColor,
-                                        fontSize: 18.55,
-                                        fontWeight: FontWeight.w600,
-                                        fontFamily: 'LemonMilkMedium'),
-                                  ),
-                                ],
-                              ),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              Text(
-                                'Meow ok how lorem ipsum vMeow ok how lorem ipsum Meow ok how lorem ipsum Meow ok how lorem ipsum Meow ok how lorem ipsum Meow ok how lorem ipsum Meow ok how lorem ipsum Meow ok how lorem ipsum Meow ok how lorem ipsum Meow ok how lorem ipsum Meow ok how lorem ipsum Meow ok how lorem ipsum Meow ok how lorem ipsum',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 17.5,
-                                    fontWeight: FontWeight.w600,
-                                    fontFamily: 'LeagueSpartan'),
-                              ),
-                              SizedBox(
-                                height: 30,
-                              ),
-                            ],
+          body: Center(
+            child: Column(
+              children: [
+                Text(
+                  'Timeline',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 56,
+                      fontFamily: 'IntroRust'),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.05,
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: AppPallete.timelineBg,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    width: 295,
+                    height: 370,
+                    child: ListView.builder(
+                      itemCount: 6,
+                      itemBuilder: (context, index) {
+                        return SingleChildScrollView(
+                          child: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    SizedBox(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.17,
+                                    ),
+                                    Text(
+                                      'Event ${index + 1}',
+                                      style: TextStyle(
+                                          color: AppPallete.timelineGold,
+                                          fontSize: 30.33,
+                                          fontWeight: FontWeight.w600,
+                                          fontFamily: 'LemonMilkMedium'),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    SizedBox(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.195,
+                                    ),
+                                    Text(
+                                      '22/2/2025',
+                                      style: TextStyle(
+                                          color: AppPallete.redColor,
+                                          fontSize: 18.55,
+                                          fontWeight: FontWeight.w600,
+                                          fontFamily: 'LemonMilkMedium'),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Text(
+                                  'Meow ok how lorem ipsum vMeow ok how lorem ipsum Meow ok how lorem ipsum Meow ok how lorem ipsum Meow ok how lorem ipsum Meow ok how lorem ipsum Meow ok how lorem ipsum Meow ok how lorem ipsum Meow ok how lorem ipsum Meow ok how lorem ipsum Meow ok how lorem ipsum Meow ok how lorem ipsum Meow ok how lorem ipsum',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 17.5,
+                                      fontWeight: FontWeight.w600,
+                                      fontFamily: 'LeagueSpartan'),
+                                ),
+                                SizedBox(
+                                  height: 30,
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                      );
-                    },
+                        );
+                      },
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.21,
-              ),
-            ],
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.21,
+                ),
+              ],
+            ),
           ),
         ),
       ),
