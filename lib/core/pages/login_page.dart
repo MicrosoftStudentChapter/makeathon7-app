51170ab61239670b7c111f813f05231fb964a2c1
Vatsal Gupta <tsrvatsal17@gmail.com>
2025-03-21 18:54:52 +0530

    login changes

--- lib/core/pages/login_page.dart
+++ lib/core/pages/login_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:makeathon7/core/pages/navigator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:makeathon7/theme/app_pallete.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   State<LoginPage> createState() => _LoginPageState();
@@ -14,37 +15,64 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _passwordController = TextEditingController();
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  bool isLoading = false;
+
   Future<void> signIn() async {
     try {
       final UserCredential user = await _auth.signInWithEmailAndPassword(
-        email: _emailController.text,
-        password: _passwordController.text,
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
       );
       if (user.user != null) {
-        Navigator.push(
-          // ignore: use_build_context_synchronously
-          context,
-          PageRouteBuilder(
-            pageBuilder: (context, animation1, animation2) =>
-                HomePageNavigator(),
-            transitionsBuilder: (context, animation1, animation2, child) {
-              return FadeTransition(
-                opacity: animation1,
-                child: child,
-              );
-            },
-            transitionDuration: Duration(milliseconds: 600),
-          ),
-        );
+        if (mounted) {
+          Navigator.push(
+            context,
+            PageRouteBuilder(
+              pageBuilder: (context, animation1, animation2) =>
+                  HomePageNavigator(),
+              transitionsBuilder: (context, animation1, animation2, child) {
+                return FadeTransition(
+                  opacity: animation1,
+                  child: child,
+                );
+              },
+              transitionDuration: Duration(milliseconds: 600),
+            ),
+          );
+        }
       }
     } catch (e) {
-      // print('Error: $e');
+      if (mounted) {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                backgroundColor: AppPallete.borderColor,
+                title: Text('Error'),
+                content: Text('Invalid Email or Password'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('OK'),
+                  ),
+                ],
+              );
+            });
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    var duration = Duration(milliseconds: 1000);
     return SafeArea(
       child: Container(
         decoration: BoxDecoration(
@@ -61,11 +89,9 @@ class _LoginPageState extends State<LoginPage> {
               Align(
                 alignment: Alignment.topLeft,
                 child: Padding(
-                 padding: const EdgeInsets.only(
-                      left: 20.0, top: 0.0),
+                  padding: const EdgeInsets.only(left: 20.0, top: 0.0),
                   child: Image.asset(
-                    'lib/assets/images/logo.png', // Path to your logo image
-                    // height: 1000, // Adjust the height as needed
+                    'lib/assets/images/logo.png',
                   ),
                 ),
               ),
@@ -118,7 +144,6 @@ class _LoginPageState extends State<LoginPage> {
                             hintStyle: TextStyle(
                               color: Colors.white.withAlpha(158),
                               fontFamily: 'LeagueSpartan',
-                             
                             ),
                             enabledBorder: OutlineInputBorder(
                               borderSide: BorderSide(color: Colors.white),
@@ -143,9 +168,9 @@ class _LoginPageState extends State<LoginPage> {
                         width: MediaQuery.of(context).size.width *
                             0.5, // Adjust the width of the login button
                         child: ElevatedButton(
-                      onPressed: () {
-                        signIn();
-                      },
+                          onPressed: () {
+                            signIn();
+                          },
                           style: ElevatedButton.styleFrom(
                             backgroundColor: const Color.fromARGB(255, 154, 7,
                                 0), // Set the background color to red
@@ -156,34 +181,42 @@ class _LoginPageState extends State<LoginPage> {
                             padding: EdgeInsets.symmetric(
                                 vertical: 10.0), // Adjust the padding
                           ),
-                          child: Text(
-                            'Login',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: 'IntroRust',
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.w900,
-                            ),
-                          ),
+                          child: isLoading
+                              ? const SizedBox(
+                                  height: 30,
+                                  width: 30,
+                                  child: CircularProgressIndicator(
+                                    color: Colors.white,
+                                  ),
+                                )
+                              : Text(
+                                  'Login',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'IntroRust',
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w900,
+                                  ),
+                                ),
                         ),
                       ),
                       SizedBox(height: 20),
                       TextButton(
                         onPressed: () {
-                          Navigator.push(
-                      context,
-                      PageRouteBuilder(
-                        pageBuilder: (context, animation1, animation2) =>
-                            HomePageNavigator(),
-                        transitionsBuilder:
-                            (context, animation1, animation2, child) {
-                          return FadeTransition(
-                            opacity: animation1,
-                            child: child,
-                          );
-                        },
-                        transitionDuration: duration,
-                      ));
+                          setState(() {
+                            isLoading = true;
+                          });
+                          signIn();
+                          Future.delayed(Duration(seconds: 3), () {
+                            setState(() {
+                              if (_emailController.text != " " ||
+                                  _passwordController.text != "") {
+                                isLoading = false;
+                              } else {
+                                isLoading = true;
+                              }
+                            });
+                          });
                         },
                         child: Text(
                           "Don't have an account?",
